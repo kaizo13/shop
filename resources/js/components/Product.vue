d43c4658db6910e53d5ce42f71d56d96514a2602
Alex <alejandrocampo@ecomputer.es>
2020-05-05 19:24:11 +0200

    Transiciones y centrar elementos de product

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <div class=" w-50">
-      <carousel class="carousel-slider" :per-page="1" :autoplay="true" :autoplayHoverPause="true" :mouse-drag="true" 
-        :loop="true" :navigation-enabled="false" :paginationEnabled="false" :autoplayTimeOut="3000" :speed="1000">
+  <div class=" h-56 ">
+    <div class="w-40 ml-8">
+      <carousel class="carousel-slider" :per-page="1" :autoplay="true" :autoplayHoverPause="true" :mouse-drag="true" :width="100"
+        :loop="true" :navigation-enabled="false" :paginationEnabled="false" :autoplayTimeOut="3000" :speed="500">
         <slide v-for="(image,index) in product.images" :key="index">
           <img :src="'/storage/' + image.img" class="w-40">
         </slide>
@@ -12,8 +12,8 @@
       <p class="bg-green-500 text-white px-5 py-1">{{product.description}} </p>
       <p class="bg-green-800 text-white px-1 py-1">{{product.price}}€</p>
     </div>
-   <div class="m-200 w-100">
-      <star-rating :rating="product.valoration" :read-only="true" :show-rating="false" :increment="0.25"
+   <div class="m-200 w-100 ml-6">
+      <star-rating  :rating="product.valoration" :read-only="true" :show-rating="false" :increment="0.25"
                   active-color="#f3bf48" :star-size="30" :padding="5"></star-rating>
     </div>
   </div>
@@ -37,3 +37,19 @@ export default {
  } 
 }
 </script>
+<style>
+
+.VueCarousel-inner {
+  transition: none !important;
+}
+
+.VueCarousel-slide {
+  transition: all 0.5s;
+  opacity: 0 !important;
+}
+
+.VueCarousel-slide-active {
+  opacity: 1 !important;
+}
+
+</style>
